ab6e61e1d303ed5e54b0394a2e49f933f84af29f
w.mutesi <willbrodmutesi@gmail.com>
2019-12-20 15:40:58 +0300

    latest version

--- oursite/static/js/index.js
+++ oursite/static/js/index.js
@@ -39,3 +39,20 @@ $('#main-slider').owlCarousel({
     autoplayHoverPause: true,
     dotsSpeed: 400,
 });
+/*
+**
+sticy nav bar
+**/
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
